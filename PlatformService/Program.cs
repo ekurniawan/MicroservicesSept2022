d94aa5db2f8f646c830afaa27ae387f2a1ad8ffb
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-09-07 14:30:53 +0700

    menambahkan httpclient access

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 //DI inject appdbcontext
 builder.Services.AddDbContext<AppDbContext>(opt=>opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient,HttpCommandDataClient>();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
